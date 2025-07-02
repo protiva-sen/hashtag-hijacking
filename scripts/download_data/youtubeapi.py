a7fcabb232e9ea3beb33a49a23448f3222b04a2e
Protiva Sen <psen1@vacc-login3.cluster>
2025-07-02 10:35:06 -0400

    store search query

--- scripts/download_data/youtubeapi.py
+++ scripts/download_data/youtubeapi.py
@@ -81,6 +81,7 @@ class YouTubeAPI:
                 channel_stats = channel_info.get("items", [{}])[0].get("statistics", {})
 
                 videos.append({
+                    'search_query': query,
                     'video_id': video_id,
                     'title': clean_text(snippet.get("title", "")),
                     'description': clean_text(snippet.get("description", "")),
@@ -116,46 +117,3 @@ class YouTubeAPI:
         return videos
     
 
-
-# from config import API_KEY
-# import requests
-
-# def fetch_comments(video_id):
-#     comments = []
-#     next_page_token = None
-
-#     while True:
-#         url = "https://www.googleapis.com/youtube/v3/commentThreads"
-#         params = {
-#             "part": "snippet",
-#             "videoId": video_id,
-#             "maxResults": 100,
-#             "textFormat": "plainText",
-#             "key": API_KEY
-#         }
-#         if next_page_token:
-#             params["pageToken"] = next_page_token
-
-#         response = requests.get(url, params=params)
-
-#         if response.status_code == 403 and 'commentsDisabled' in response.text:
-#             print(f"Comments are disabled for video: {video_id}")
-#             comments.append((f"disabled-{video_id}", video_id, "disabled", "", ""))
-#             break
-
-#         data = response.json()
-#         for item in data.get("items", []):
-#             snippet = item["snippet"]["topLevelComment"]["snippet"]
-#             comments.append((
-#                 item["id"],
-#                 video_id,
-#                 snippet.get("textDisplay", ""),
-#                 snippet.get("authorDisplayName", ""),
-#                 snippet.get("publishedAt", "")
-#             ))
-
-#         next_page_token = data.get("nextPageToken")
-#         if not next_page_token:
-#             break
-
-#     return comments
