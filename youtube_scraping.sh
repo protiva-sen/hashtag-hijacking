d029787aa79d18ce5ceb95127e7ea5984c8f5914
Aviral Chawla <avi.champ@hotmail.com>
2025-07-03 11:45:57 -0400

    updated bash

--- youtube_scraping.sh
+++ youtube_scraping.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-#sbatch --job-name=youtube_data_collection
-#sbatch --output=yt_data_collection.out
-#sbatch --error=yt_data_collection.err
-#sbatch --time=5:00:00
-#sbatch --partition=general
-#sbatch --nodes=1
-#sbatch --mem=8G
-#sbatch --ntasks=1
-#sbatch --mail-type=END,FAIL
-#sbatch --mail-user=psen1@uvm.edu
+#SBATCH --job-name=youtube_data_collection
+#SBATCH --output=yt_data_collection.out
+#SBATCH --error=yt_data_collection.err
+#SBATCH --time=5:00:00
+#SBATCH --partition=general
+#SBATCH --nodes=1
+#SBATCH --mem=30G
+#SBATCH --ntasks=1
+#SBATCH --mail-type=END,FAIL
 
 
-source ~/anaconda3/etc/profile.d/conda.sh
-conda activate /gpfs1/home/p/s/psen1/hashtag-hijacking/.conda
+source ~/.bash_profile
+conda hashtag-hijacking
 
-cd /gpfs1/home/p/s/psen1/hashtag-hijacking
+cd scripts/download_data
 
-python scripts/download_data/main.py \
+python main.py \
     --start_date 2023-07-01T00:00 \
     --end_date 2025-07-01T00:00  \
     --queries data/hashtags.txt \
     --api_key AIzaSyDz-hIiz3-c2D73yAl6BWvr-Y1k71KZtV0
+
+
