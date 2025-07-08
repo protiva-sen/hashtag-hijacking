8d9ae4a4f9c58b0301eb235f8e4fc609fd286a22
Aviral Chawla <avi.champ@hotmail.com>
2025-07-08 15:10:52 -0400

    minor change

--- scripts/download_data/youtube_scraping.sh
+++ scripts/download_data/youtube_scraping.sh
@@ -3,12 +3,11 @@
 #SBATCH --job-name=youtube_data_collection
 #SBATCH --output=yt_data_collection.out
 #SBATCH --error=yt_data_collection.err
-#SBATCH --time=5:00:00
+#SBATCH --time=10:00:00
 #SBATCH --partition=general
 #SBATCH --nodes=1
 #SBATCH --mem=30G
 #SBATCH --ntasks=1
-#SBATCH --mail-type=END,FAIL
 
 
 source ~/.bash_profile
